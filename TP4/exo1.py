71dcd730b9b7c728a752efb40d2c056854eb7d4b
Mathis Gorvien <mathis.gorvien@gmail.com>
2022-05-17 08:58:55 +0200

    ok

--- TP4/exo1.py
+++ TP4/exo1.py
@@ -5,7 +5,6 @@ import sys
 import multiprocessing as mp
 
 liste=[1,68,5,5,26,5,98,4,5,1,47,2,3,14,8,99,2]
-n=len(liste)
 
 sem = mp.Semaphore(1)
 mutex = mp.Lock()
@@ -13,30 +12,31 @@ Somme = mp.Value('i', 0)
 
 #Processus P1
 def fP1(pliste):
-    SommeImpairs=0
-    for i in range(n-1):
-        SommeImpairs+=pliste[2*n+1]
-
+    n=len(pliste)
+    j = 1
+    SommeImpairs = 0
+    while j <= n-1:
+        SommeImpairs += pliste[j]
+        j += 2
     sem.acquire()
     mutex.acquire()
-
-    Somme+=SommeImpairs
-
+    Somme.value += SommeImpairs
     mutex.release()
     sem.release()
     sys.exit(0)
 
+
 #Processus P2
 def fP2(pliste):
-    SommePairs=0
-    for i in range(n-1):
-        SommePairs+=pliste[2*n]
-    
+    n=len(pliste)
+    i = 0
+    SommePairs = 0
+    while i <= n:
+        SommePairs += pliste[i]
+        i += 2
     sem.acquire()
     mutex.acquire()
-
-    Somme+=SommePairs
-
+    Somme.value += SommePairs
     mutex.release()
     sem.release()
     sys.exit(0)
